446bbbb39228e69ae3aaf1805d43db2bcc63a788
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-07 20:52:27 +0800

    modify: hand in exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,18 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array == null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const re = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > 0) {
+      for (let j = 0; j < array[i].length; j += 1) {
+        re.push(array[i][j]);
+      }
+    } else {
+      re.push(array[i]);
+    }
+  }
+  return re;
 }
