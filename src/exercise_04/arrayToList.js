ecb86697e7216335c13ae7fce8e03f9cce328b9d
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-07 17:48:39 +0800

    modify: hand in exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,28 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array == null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  const head = {};
+  let cur = null;
+  let pre = null;
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === 0) {
+      head.value = array[i];
+      head.next = cur;
+      pre = head;
+    } else {
+      cur = {};
+      cur.value = array[i];
+      cur.next = null;
+      pre.next = cur;
+      pre = cur;
+      cur = cur.next;
+    }
+  }
+  return head;
 }
