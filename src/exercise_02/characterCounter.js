0e6d8c863a9ae0bb1234431004a81aadb3ba3b5c
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-07 14:52:11 +0800

    modify: hand in characterCounter.js

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,17 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string == null || string === '') {
+    return 0;
+  }
+  if (prediction === undefined) {
+    return string.length;
+  }
+  let num = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i])) {
+      num += 1;
+    }
+  }
+  return num;
 }
